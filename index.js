cd88c85f1308fd7cbfddefde04d2dcaf1769f506
Seweryn Kimla <sewkim@vp.pl>
2018-07-31 18:25:11 +0200

    Modify route

--- index.js
+++ index.js
@@ -9,6 +9,7 @@ const app = express();
 //Make connect to MongoDB
 
 mongoose.connect('mongodb://localhost/character');
+
 mongoose.Promise = global.Promise;
 
 app.get('/api', function(req,res){
@@ -21,6 +22,13 @@ app.get('/api', function(req,res){
 app.use(bodyParser.json());
 
 app.use('/api', require('./routes/api'));
+
+//Error Middleware handling function
+app.use(function(err,req,res,next){
+    // console.log(err);
+    //Response from server, we can display this message for user of the Front-end App
+    res.status(422).send({ error: err.message});
+})
 
 //Listen requests
 app.listen(process.env.port || 4000, function(){
--- routes/api.js
+++ routes/api.js
@@ -3,29 +3,46 @@ const route = express.Router();
 const Character = require('../models/character')
 
 
-route.get('/characters', function(req,res){
+route.get('/characters', function(req,res,next){
     res.send({type: 'GET'})
 });
 
 //ADD CHARACTER TO DB
-route.post('/characters', function(req,res){
+route.post('/characters', function(req,res,next){
     // console.log(req.body);
-    Character.create(req.body);
-    
-    res.send({
-        type: 'POST',
-        name: req.body.name,
-        rank: req.body.rank
-    });
+    Character.create(req.body)
+    .then(function(character){
+        res.send(character);
+    })
+    .catch(next)
+
+    // res.send({
+    //     type: 'POST',
+    //     name: req.body.name,
+    //     rank: req.body.rank
+    // });
 });
 
 //UPDATE CHARACTER IN DB
-route.put('/characters/:id', function(req,res){
-    res.send({type: 'PUT'})
+route.put('/characters/:id', function(req,res,next){
+    res.send({type: 'PUT'});
+    Character.findByIdAndUpdate({_id: req.params.id}, req.body)
+    .then(function(){
+        Character.findOne({_id: req.params.id})
+        .then(function(character){
+            res.send(character);
+        })
+    })
 });
 
 //DELETE CHARACTER FROM DB
-route.delete('/characters/:id', function(req,res){
+route.delete('/characters/:id', function(req,res,next){
+    //console.log(req) req is Object and has params property.
+    // console.log(req.params.id)
+    Character.findByIdAndRemove({_id: req.params.id})
+    .then(function(character){
+        res.send(character)
+    })
     res.send({type: 'DELETE'})
 });
 
